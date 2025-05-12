de96383900c3e909a0c43cdf9ae5505d225d647e
haniebayati <haniye.bayati79@gmail.com>
2025-05-13 00:21:38 +0330

    Injected realm from properties file into service using @Value annotation

--- src/main/java/com/example/user_management/service/KeycloakUserService.java
+++ src/main/java/com/example/user_management/service/KeycloakUserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,8 @@ public class KeycloakUserService {
 
     private final Keycloak keycloak;
     
-    private String realm = "user-management";
+    @Value("${keycloak.personal.realm}")
+    private String realm;
 
     public KeycloakUserService(Keycloak keycloak) {
         this.keycloak = keycloak;
